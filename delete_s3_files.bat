8a458361940567b737ad525c79a8c8b02c77a358
JACJava <46228469+JACJava@users.noreply.github.com>
2019-10-31 13:25:59 -0500

    S3 bat files

---
+++ delete_s3_files.bat
@@ -0,0 +1,3 @@
+echo Deleting S3 Files
+
+aws s3 rm s3://hamster-bucket-julieawscf/ --recursive
--- load_s3_files.bat
+++ load_s3_files.bat
@@ -1,3 +1,3 @@
 echo Loading S3 Files
 
-aws s3 cp C:\Users\Julie\Documents\GitHub\hbfl-master\public s3://hamster-bucket-julieawscf/ --recursive
+aws s3 cp C:\Users\Julie\Documents\GitHub\hbfl-master\public s3://hamster-bucket-julieawscf/ --recursive --acl public-read
